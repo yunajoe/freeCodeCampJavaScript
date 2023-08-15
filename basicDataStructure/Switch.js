a251dcd640f663cc426571f151cb2ebf414034fc
yunajoe <yunajoe@gmail.com>
2023-08-15 12:40:20 +0900

    Switch.js

---
+++ basicDataStructure/Switch.js
@@ -0,0 +1,69 @@
+//
+function caseInSwitch(val) {
+  let answer = "";
+  // Only change code below this line
+  switch (val) {
+    case 1:
+      answer = "alpha";
+      break;
+    case 2:
+      answer = "beta";
+      break;
+    case 3:
+      answer = "gamma";
+      break;
+    case 4:
+      answer = "delta";
+      break;
+  }
+  // Only change code above this line
+  return answer;
+}
+
+// Adding a Default Option in Switch Statements
+
+function switchOfStuff(val) {
+  let answer = "";
+  // Only change code below this line
+  switch (val) {
+    case "a":
+      answer = "apple";
+      break;
+    case "b":
+      answer = "bird";
+      break;
+    case "c":
+      answer = "cat";
+      break;
+    default:
+      answer = "stuff";
+      break;
+  }
+  // Only change code above this line
+  return answer;
+}
+
+// Multiple Identical Options in Switch Statements
+function sequentialSizes(val) {
+  let answer = "";
+  // Only change code below this line
+  switch (val) {
+    case 1:
+    case 2:
+    case 3:
+      answer = "Low";
+      break;
+    case 4:
+    case 5:
+    case 6:
+      answer = "Mid";
+      break;
+    case 7:
+    case 8:
+    case 9:
+      answer = "High";
+      break;
+  }
+  // Only change code above this line
+  return answer;
+}
